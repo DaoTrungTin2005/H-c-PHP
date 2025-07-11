6103f1568c81e2176f2c8f88b9ff58cc67d590cc
Tín <Trungtin0701200@gmail.com>
2025-07-11 09:22:45 +0700

    Update được thông tin user

--- Baihoc/Chap21/PHP_Mysql/modules/users/update.php
+++ Baihoc/Chap21/PHP_Mysql/modules/users/update.php
@@ -1,6 +1,43 @@
 <?php
 $id = (int)$_GET['id'];
 
+
+?>
+
+
+<?php
+if (isset($_POST['btn_update'])) {
+    $error = array();
+    $alert = array();
+    #Kiểm tra fullname
+
+    if (empty($_POST['fullname'])) {
+        $error['fullname'] = "Không được để trồng fullname";
+    } else {
+        $fullname = $_POST['fullname'];
+    }
+
+    #Kiểm tra giới tính
+    if (empty($_POST['gender'])) {
+        $error['gender'] = "Bạn cần chọn giới tính";
+    } else {
+        $gender = $_POST['gender'];
+    }
+
+
+    // Nếu không có lỗi thì update
+    if (empty($error)) {
+        $sql = "UPDATE tbl_user SET fullname ='{$fullname}', gender ='{$gender}' WHERE id= {$id}";
+
+        //Đây là hàm thực thi câu lệnh SQL trong PHP dùng MySQLi.
+        if (mysqli_query($conn, $sql)) {
+            echo "Update dl thanh cong";
+        }
+    } else {
+    }
+}
+
+// nói chung là cái này để Lấy thông tin của người dùng từ database ra để update (la để hiện thông tin cũ của ng cần update rồi mói update(hieen ở form á))
 $sql = "SELECT * FROM tbl_user WHERE id = $id";
 
 // Gửi câu lệnh SQL lên CSDL (MySQL) bằng hàm mysqli_query.
@@ -24,3 +61,34 @@ $result = mysqli_query($conn, $sql);
 $item = mysqli_fetch_array($result);
 
 show_array($item);
+?>
+
+<div id="content">
+    <h2 style="text-align:center">Sửa du lieu</h2>
+    <?php
+
+    ?>
+    <form action="" method="POST">
+        <label for="fullname">Họ và tên</label>
+        <input type="text" name="fullname" id="fullname"
+            value="<?php echo isset($item['fullname']) ? $item['fullname'] : ''; ?>">
+        <?php echo isset($error['fullname']) ? '<p style="color:red;">' . $error['fullname'] . '</p>' : ''; ?>
+
+
+        <label for="gender">Giới tính</label>
+        <?php
+        // Nếu đã submit thì dùng giá trị đã chọn, ngược lại dùng giá trị từ CSDL
+        $current_gender = isset($_POST['gender']) ? $_POST['gender'] : $item['gender'];
+        ?>
+        <select name="gender" id="gender">
+            <option value="">--Chọn giới tính--</option>
+            <option value="male" <?php echo ($current_gender == 'male') ? 'selected' : ''; ?>>Nam</option>
+            <option value="female" <?php echo ($current_gender == 'female') ? 'selected' : ''; ?>>Nữ</option>
+            <option value="other" <?php echo ($current_gender == 'other') ? 'selected' : ''; ?>>Khác</option>
+        </select>
+        <?php echo isset($error['gender']) ? '<p style="color:red;">' . $error['gender'] . '</p>' : ''; ?>
+
+        <br><br>
+        <input type="submit" name="btn_update" value="CAP NHAT">
+    </form>
+</div>
